941cacf9b1682ab935324be27d85a39bcc78d781
Jason Sipula <alupis1@gmail.com>
2015-02-24 14:14:16 -0800

    update build script

--- build.sh
+++ build.sh
@@ -155,7 +155,7 @@ fi
 ### Validation (decide what's required for running your script and error out)
 #####################################################################
 
-[ -z "${arg_f}" ]     && help      "Setting a filename with -f is required"
+#[ -z "${arg_f}" ]     && help      "Setting a filename with -f is required"
 [ -z "${LOG_LEVEL}" ] && emergency "Cannot continue without LOG_LEVEL. "
 
 
@@ -176,15 +176,6 @@ if [[ "${OSTYPE}" == "darwin"* ]]; then
   info "You are on OSX"
 fi
 
-debug "Info useful to developers for debugging the application, not useful during operations."
-info "Normal operational messages - may be harvested for reporting, measuring throughput, etc. - no action required."
-notice "Events that are unusual but not error conditions - might be summarized in an email to developers or admins to spot potential problems - no immediate action required."
-warning "Warning messages, not an error, but indication that an error will occur if action is not taken, e.g. file system 85% full - each item must be resolved within a given time. This is a debug message"
-error "Non-urgent failures, these should be relayed to developers or admins; each item must be resolved within a given time."
-critical "Should be corrected immediately, but indicates failure in a primary system, an example is a loss of a backup ISP connection."
-alert "Should be corrected immediately, therefore notify staff who can fix the problem. An example would be the loss of a primary ISP connection."
-emergency "A \"panic\" condition usually affecting multiple apps/servers/sites. At this level it would usually notify all tech staff on call."
-
 ### Begin Program
 #####################################################################
 if [ ! -d "target" ]; then
@@ -192,16 +183,17 @@ if [ ! -d "target" ]; then
     mkdir -pv target
 fi
 
-cd -v target/
+cd target/
 
 info "Cloning latest crosstool-ng"
 if [ ! -d "crosstool-ng" ]; then
     git clone https://github.com/crosstool-ng/crosstool-ng --depth=1
 fi
-cd -v crosstool-ng/
+cd crosstool-ng/
 git fetch
 git reset --hard "origin/master"
 
+./bootstrap
 ./configure --prefix="${__dir}/target/crosstool-ng-install"
 make
 make install
